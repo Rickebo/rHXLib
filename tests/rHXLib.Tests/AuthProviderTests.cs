74c5512b38e5ef43f3d17df80fda4771f14f7f09
Simon Pettersson <rickebo@rickebo.com>
2025-11-01 21:16:00 +0100

    test(auth): add static token and launcher file provider tests

---
+++ tests/rHXLib.Tests/AuthProviderTests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace rHXLib.Tests;
+
+public class AuthProviderTests
+{
+    [Fact]
+    public async Task StaticToken_Returns_Value()
+    {
+        var p = new StaticTokenProvider("abc");
+        var t = await p.GetAccessTokenAsync();
+        t.Should().Be("abc");
+    }
+
+    [Fact]
+    public async Task LauncherFile_Reads_access_token_From_Json()
+    {
+        var tmp = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".json");
+        try
+        {
+            await File.WriteAllTextAsync(tmp, "{\"access_token\":\"tok123\"}");
+            var p = new LauncherFileTokenProvider(tmp);
+            var t = await p.GetAccessTokenAsync();
+            t.Should().Be("tok123");
+        }
+        finally
+        {
+            try { File.Delete(tmp); } catch { }
+        }
+    }
+}
+
